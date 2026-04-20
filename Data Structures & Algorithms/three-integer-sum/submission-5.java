e0fe4466714e24576817c8fee1376a914433d9da
Ranjana Bongale Ganesh <49183193+ranjanabg@users.noreply.github.com>
2026-04-20 12:00:27 -0700

    Add: three-integer-sum - submission-5

---
+++ Data Structures & Algorithms/three-integer-sum/submission-5.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> uniqueTriplets = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                return uniqueTriplets;
+            }
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int l = i + 1;
+            int r = nums.length - 1;
+
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum == 0) {
+                    uniqueTriplets.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    l++;
+                    r--;
+                    while (l < r && nums[l] == nums[l - 1]) {
+                        l++;
+                    }
+                } else if (sum > 0) {
+                    r--;
+                } else {
+                    l++;
+                }
+            }
+        }
+
+        return uniqueTriplets;
+    }
+
+    /* Notes: Common pitfall is not skipping duplicate values. Duplicate or identical values for 1st element or Outer loop and l pointer must be skipped.
+    Failing to skip these elements would result in duplicate arrays */
+
+}
